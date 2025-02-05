ab7c47f3bab94981056e02ee94f3e0c10d7daffc
aatikpohou <aatikpohou@unomaha.edu>
2025-02-05 16:21:03 +0000

    done

--- ApproxPi.py
+++ ApproxPi.py
@@ -13,15 +13,23 @@ def main():
 
   #ask user for decimal percision (up to 10)
 
+  approxPi = 4/1
+sign = -1
+denom = 3
+for i in range(100):
+  print(approxPi)
+  
   start = time.time()
+  while round(approxPi, 7)  != round(realPi, 7):
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
    
   
-print(ApproxPi)
-end = time.time()
+ #print(ApproxPi)
+    approxPi = approxPi + sign * 4/denom 
+  end = time.time()
 
-  elapsedTime = end - start
+  
   print(elapsedTime)
 
 if __name__ == '__main__':
--- RPS.py
+++ RPS.py
@@ -10,13 +10,70 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  play = "Y"
+  while play == "Y":
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    #Prompt the user for their RPS selection
+    #Determine winner and state what happened to the user
+    player = input("Enter choice (R/P/S): ")
+    computer = random.choice(["R", "P", "S"] )
+    if computer == "R":
+      print ("Computer chose Rock")
+    elif computer == "P":
+      print("Computer chose Paper")
+    else:
+      print("Computer chose Scissors")
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
+    if player == "R":
+      print ("Player chose Rock")
+    elif player == "P":
+      print("Player chose Paper")
+    else:
+      print("Player chose Scissors")
   #Ask the user if they would like to play again.
 
-  #In the end, print the stats
+    
+
+    if player == "R":
+      if computer == "R":
+        print("TIE")
+        ties = ties + 1
+
+      if computer =="P":
+        print("You lose")
+        losses = losses + 1 
+
+      if computer == "S":
+        print ("You win")
+        wins = wins + 1
+
+    if player == "P":
+      if computer == "P":
+        print("TIE")
+        ties = ties + 1
+
+      if computer =="S":
+        print("You lose")
+        losses = losses + 1 
+
+      if computer == "R":
+        print ("You win")
+        wins = wins + 1
+      
+    if player == "S":
+      if computer == "S":
+        print("TIE")
+        ties = ties + 1
+
+      if computer =="R":
+        print("You lose")
+        losses = losses + 1 
+
+      if computer == "P":
+        print ("You win")
+        wins = wins + 1
+    play = input("Play again? (Y/N): ")
+    #In the end, print the stats
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
   print(wins, "\t", ties , "\t", losses)
